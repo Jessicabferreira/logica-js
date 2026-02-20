9892b105067076976c411d778459f3b5aaeee8fc
Jessica <jessiberteloni@gmail.com>
2026-02-20 08:59:37 -0300

    Número aleatório

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
+let numeroSecreto = parceInt(Math.random() * 100 + 1);
 console.log('numeroSecreto');
 let chute;
 let tentativas = 1;
 
 // Enquanto chute não for igual ao número secreto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     // Se o chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
         if (chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
-            tentativas++
+        // tentativas = tentativas + 1
+        tentativas++
     }
 }
+
+let palavratentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavratentativa}.`);
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+// } else{
+//      alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+// }
